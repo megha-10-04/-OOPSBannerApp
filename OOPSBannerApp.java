b1d58fc6d27d07fbdcbc2db4f507544b0de07df0
megha-10-04 <meghakakkar2007@gmail.com>
2026-03-17 09:10:29 +0530

    UC3: Refactored using String.join() for better efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,33 @@
 /**
  * OOPSBannerApp
- * UC2: Print OOPS in Banner Format using *
+ * UC3: Banner using String.join()
  * 
  * @author Megha
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" ********   ********   ********   ******** ");
-        System.out.println(" *      *   *      *   *      *   *        ");
-        System.out.println(" *      *   *      *   *      *   *        ");
-        System.out.println(" *      *   *      *   ********   ******** ");
-        System.out.println(" *      *   *      *   *          *        ");
-        System.out.println(" *      *   *      *   *          *        ");
-        System.out.println(" ********   ********   *          ******** ");
+        System.out.println(String.join(" ",
+                "********", "  ", "********", "  ", "********", "  ", "********"));
+
+        System.out.println(String.join(" ",
+                "*      *", "  ", "*      *", "  ", "*      *", "  ", "*       "));
+
+        System.out.println(String.join(" ",
+                "*      *", "  ", "*      *", "  ", "*      *", "  ", "*       "));
+
+        System.out.println(String.join(" ",
+                "*      *", "  ", "*      *", "  ", "********", "  ", "********"));
+
+        System.out.println(String.join(" ",
+                "*      *", "  ", "*      *", "  ", "*       ", "  ", "*       "));
+
+        System.out.println(String.join(" ",
+                "*      *", "  ", "*      *", "  ", "*       ", "  ", "*       "));
+
+        System.out.println(String.join(" ",
+                "********", "  ", "********", "  ", "*       ", "  ", "********"));
     }
 }
