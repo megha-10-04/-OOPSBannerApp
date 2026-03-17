0a8fc6d736ef426066dded31218eacedbde056f9
megha-10-04 <meghakakkar2007@gmail.com>
2026-03-17 09:11:57 +0530

    UC4: Used String array and loop for banner printing

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,29 @@
 /**
  * OOPSBannerApp
- * UC3: Banner using String.join()
+ * UC4: Banner using String Array and Loop
  * 
  * @author Megha
- * @version 3.0
+ * @version 4.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
-                "********", "  ", "********", "  ", "********", "  ", "********"));
+        // Step 1: Create array to store banner lines
+        String[] banner = new String[7];
 
-        System.out.println(String.join(" ",
-                "*      *", "  ", "*      *", "  ", "*      *", "  ", "*       "));
+        // Step 2: Fill array using String.join()
+        banner[0] = String.join(" ", "********", "  ", "********", "  ", "********", "  ", "********");
+        banner[1] = String.join(" ", "*      *", "  ", "*      *", "  ", "*      *", "  ", "*       ");
+        banner[2] = String.join(" ", "*      *", "  ", "*      *", "  ", "*      *", "  ", "*       ");
+        banner[3] = String.join(" ", "*      *", "  ", "*      *", "  ", "********", "  ", "********");
+        banner[4] = String.join(" ", "*      *", "  ", "*      *", "  ", "*       ", "  ", "*       ");
+        banner[5] = String.join(" ", "*      *", "  ", "*      *", "  ", "*       ", "  ", "*       ");
+        banner[6] = String.join(" ", "********", "  ", "********", "  ", "*       ", "  ", "********");
 
-        System.out.println(String.join(" ",
-                "*      *", "  ", "*      *", "  ", "*      *", "  ", "*       "));
-
-        System.out.println(String.join(" ",
-                "*      *", "  ", "*      *", "  ", "********", "  ", "********"));
-
-        System.out.println(String.join(" ",
-                "*      *", "  ", "*      *", "  ", "*       ", "  ", "*       "));
-
-        System.out.println(String.join(" ",
-                "*      *", "  ", "*      *", "  ", "*       ", "  ", "*       "));
-
-        System.out.println(String.join(" ",
-                "********", "  ", "********", "  ", "*       ", "  ", "********"));
+        // Step 3: Print using loop (NO repetition)
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
